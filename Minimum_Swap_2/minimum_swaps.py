afbfecbd8cbbaa4520c4f029922a4a76a8a1fe13
Shola Ayeni <ayenisholah@yahoo.com>
2019-11-26 15:42:31 +0100

    minimum swap 2 solution in python

---
+++ Minimum_Swap_2/minimum_swaps.py
@@ -0,0 +1,32 @@
+def minSwaps(arr):
+    counter = 0
+
+    length = len(arr)
+
+    arr_dict = {}
+
+    for i, item in enumerate(arr):
+        arr_dict[item - 1] = i
+
+    checked = [False] * length
+
+    for key, value in sorted(arr_dict.items(), key=lambda x: x[1]):
+
+        if checked[key] or key == value:
+            continue
+
+        c_count = 0
+        value = key
+
+        while not checked[value]:
+
+            checked[value] = True
+            value = arr_dict[value]
+            c_count += 1
+
+        counter += c_count - 1
+
+    return counter
+
+
+print(minSwaps([2, 3, 4, 1, 5]))
